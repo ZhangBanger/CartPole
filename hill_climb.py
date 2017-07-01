587ac31c8e0ec68553f7d2810908c94905ed3a47
Andy Zhang <signupforandy@gmail.com>
2017-06-30 20:17:17 -0700

    Fix noise term (uniform -> gaussian) and annealing
    * Start with lower noise factor and stop training in 500 evals

--- hill_climb.py
+++ hill_climb.py
@@ -2,8 +2,13 @@ import gym
 import numpy as np
 from gym.wrappers.monitoring import Monitor
 
+MC_POLICY_EVAL_EP = 10
+BASE_NOISE_FACTOR = 0.5
+NUM_POLICY_EVAL = 500
+
+
 env = gym.make('CartPole-v0')
-env = Monitor(env, 'tmp/cart-pole-hill-climb-2', force=True)
+env = Monitor(env, 'tmp/cart-pole-hill-climb-3', force=True)
 
 print("Action space: {0}".format(env.action_space))
 print("Observation space: {0}\n\tLow: {1}\n\tHigh: {2}".format(
@@ -54,28 +59,28 @@ def evaluate_policy(num_episodes, weights):
 
 best_reward = -np.inf
 best_params = np.random.rand(4) * 2 - 1
-num_eval_eps = 10
-base_noise_factor = 1.
-num_evaluations = 1000
 
 print("Running Hill Climb on Cart Pole")
 print("Params:\n\tMC Eval Count: {0} trajectories\n\tBase Noise Factor: {1}".format(
-    num_eval_eps,
-    base_noise_factor,
+    MC_POLICY_EVAL_EP,
+    BASE_NOISE_FACTOR,
 ))
 
-for i_episode in range(num_evaluations):
+for i_episode in range(NUM_POLICY_EVAL):
     # Weights are 1x4 matrix
     # µ = 0 , sigma 1
-    noise_scaling = 1 - (i_episode / num_evaluations)
+    annealing_term = 1 - (i_episode / NUM_POLICY_EVAL)
+    noise_scaling = BASE_NOISE_FACTOR * annealing_term
     print("Applying jitter with factor {0} to parameters {1}".format(
         noise_scaling,
         best_params,
     ))
 
-    noise_term = (np.random.rand(4) * 2 - 1) * noise_scaling
+    # Add gaussian noise
+    # µ = 0 , sigma = noise_scaling
+    noise_term = np.random.randn(4) * noise_scaling
     parameters = best_params + noise_term
-    episodic_reward = evaluate_policy(num_eval_eps, parameters)
+    episodic_reward = evaluate_policy(MC_POLICY_EVAL_EP, parameters)
     if episodic_reward > best_reward:
         print("Episode {2}: Got new best reward of {0}, better than previous of {1}".format(
             episodic_reward,
